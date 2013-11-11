f6585e7a6d3a418685db7b4b5864534d8392dd7c
Erik Mellum <mellum.erik@gmail.com>
2013-11-10 16:08:37 -0800

    Attempted Updated

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,32 +5,27 @@ $(document).ready(function(){
 	$('.TopNavButton').mouseleave(function(){
 		$(this).animate({'opacity':'1'}, 1);
 	});
-    $('.BodyContent').hover(function(){
-		$(this).animate({'opacity':'0.5'}, 1);
-	});
-	$('.BodyContent').mouseleave(function(){
-		$(this).animate({'opacity':'1'}, 1);
-	});
+    
     function checkWindowSize() {
-        
-        
-        if ( $(window).width() < 500 ) {
+
+        if ( $(window).width() < 600 ) {
+            $('#rightContent').hide();
+            $('#leftAside').hide();
             $('#topHeader').hide();
-            $('.NavHeader').fitText(.6);
-            $('.BodyHeader').fitText(2);
-            $('.PostHeader').fitText(.8);
-            $('p').fitText(1.3);
+            $('#body').css({'left' : '5%', 'right' : '5%', 'width' : '90%'});
+            $('#leftContent').css({width: '100%', 'right' : '0'});
         }
         else {
+            $('#leftAside').show();
             $('#topHeader').show();
-            $('.NavHeader').fitText(.6);
-            $('.BodyHeader').fitText(2);
-            $('.PostHeader').fitText(.8);
-            $('p').fitText(1.3);
+            $('#rightContent').show();
+            $('#leftContent').css({'right' : '40%', width: '55%'});
+            $('#body').css({'left' : '30%', 'right' :'5%', 'width' : '65%'});
         }
 
     }
     
 $(window).load(checkWindowSize);
 $(window).resize(checkWindowSize);
+   
 });
